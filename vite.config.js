a1d747f8f9183c1236be7d37e5b8f0bdfe4b12a2
ADITHIYAN P V <123310700+Adithiyanpv@users.noreply.github.com>
2024-11-25 23:36:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: import.meta.env.PORT || 4173,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
     host: '0.0.0.0'
   }
 });
